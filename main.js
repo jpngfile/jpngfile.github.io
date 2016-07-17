d10aeeb106096a02805b02816ffa889c2672deca
Jason P'ng <jsjpng@gmail.com>
2016-07-16 22:07:20 -0400

    Added portfolio and resume page

--- main.js
+++ main.js
@@ -23,4 +23,25 @@
 	}
 })(window,document);
 
+function changeResumeType(){
+	console.log ("Change resume type");
+	var type = document.getElementById("resumeType").value;
+	var pdf = document.getElementById("resumePdf");
+	var json = document.getElementById("resumeJson");
+	var png = document.getElementById("resumePng");
+	if (type == "pdf"){
+		pdf.style.display = "block";
+		json.style.display = "none";
+		png.style.display = "none";
+	} else if (type == "json") {
+		pdf.style.display = "none";
+		json.style.display = "block";
+		png.style.display = "none";
+	} else if (type == "png") {
+		pdf.style.display = "none";
+		json.style.display = "none";
+		png.style.display = "block";
+	}
+}
+
 Window.init();
